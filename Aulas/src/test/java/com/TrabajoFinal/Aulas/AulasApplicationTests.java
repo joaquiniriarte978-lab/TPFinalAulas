620861022f27e4415c251cecd8807ca79307ff0d
Joaquin Iriarte <joaquiniriarte978@gmail.com>
2026-04-27 08:59:30 -0300

    Empezamos

---
+++ Aulas/src/test/java/com/TrabajoFinal/Aulas/AulasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrabajoFinal.Aulas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
