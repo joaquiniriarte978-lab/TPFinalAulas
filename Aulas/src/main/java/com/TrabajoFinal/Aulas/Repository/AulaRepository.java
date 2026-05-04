adf545cda3782fee15bc33952d24f3d9102331e1
roquevisciarelli <rvisciarelli2@gmail.com>
2026-05-04 09:58:42 -0300

    Repositorios de Aulas y Aviso

--- Aulas/src/main/java/com/TrabajoFinal/Aulas/Repository/AulaRepository.java
+++ Aulas/src/main/java/com/TrabajoFinal/Aulas/Repository/AulaRepository.java
@@ -2,6 +2,22 @@ package com.TrabajoFinal.Aulas.Repository;
 
 import com.TrabajoFinal.Aulas.model.Aula;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface AulaRepository extends JpaRepository<Aula,Integer> {
+
+
+
+    public List<Aula> findAll();
+
+    public List<Aula> findAulaById_aula(Integer idaula);
+
+    public void saveAula(Aula aula);
+
+    public void deleteAulaById(Integer idaula);
+
+    public void updateAulaById(Integer idaula, Aula aula);
+
 }
--- Aulas/src/main/java/com/TrabajoFinal/Aulas/Repository/AvisoRepository.java
+++ Aulas/src/main/java/com/TrabajoFinal/Aulas/Repository/AvisoRepository.java
@@ -1,7 +1,22 @@
 package com.TrabajoFinal.Aulas.Repository;
 
+import com.TrabajoFinal.Aulas.model.Aula;
 import com.TrabajoFinal.Aulas.model.Aviso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AvisoRepository extends JpaRepository<Aviso,Integer> {
+
+    public List<Aviso> findAll();
+
+    public List<Aviso> findAvisoById_aviso(Integer idaviso);
+
+    public void saveAviso(Aviso aviso);
+
+    public void deleteAvisoById(Integer idaviso);
+
+    public void updateAvisoById(Integer idaviso, Aviso aviso);
+
+
 }
