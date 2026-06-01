192806a22568eeb01118652000cd9de9cb9ee13b
mateoasaro <mateo.asaro123@gmail.com>
2026-06-01 09:38:44 -0300

    mateo clase aulaService y aulaRepository

--- Aulas/src/main/java/com/TrabajoFinal/Aulas/Repository/AulaRepository.java
+++ Aulas/src/main/java/com/TrabajoFinal/Aulas/Repository/AulaRepository.java
@@ -1,5 +1,6 @@
 package com.TrabajoFinal.Aulas.Repository;
 
+import com.TrabajoFinal.Aulas.model.Aula;
 import com.TrabajoFinal.Aulas.model.enums.Tipo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,15 +9,9 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface AulaRepository extends JpaRepository {
+public interface AulaRepository extends JpaRepository<Aula, Integer> {
 
 
-    public Optional findAulaById_aula(Integer idaula);
 
-    public List findAulasByCapacidad(int capacidad);
-
-    public List findAulasByNombre(String nombre);
-
-    public List findAulasByTipo(Tipo tipo);
 
 }
