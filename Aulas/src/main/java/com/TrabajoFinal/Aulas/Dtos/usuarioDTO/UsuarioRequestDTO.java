09ed3d8979a133c0cdf1ee5d6930d6b9fb17cd9b
roquevisciarelli <rvisciarelli2@gmail.com>
2026-06-16 12:52:49 -0300

    "validaciones basicas para usuarios y comisiones"

--- Aulas/src/main/java/com/TrabajoFinal/Aulas/Dtos/usuarioDTO/UsuarioRequestDTO.java
+++ Aulas/src/main/java/com/TrabajoFinal/Aulas/Dtos/usuarioDTO/UsuarioRequestDTO.java
@@ -1,19 +1,21 @@
 package com.TrabajoFinal.Aulas.Dtos.usuarioDTO;
 
-
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UsuarioRequestDTO {
 
     @NotBlank(message = "El nombre es obligatorio")
+    @Size(max = 20, message = "El nombre debe tener un máximo de 20 caracteres")
     private String nombre;
 
-    @Email(message = "Email inválido")
     @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Email inválido")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "Debe ingresar un correo válido con su dominio (ej: @gmail.com, @hotmail.com)")
     private String email;
 
     @NotBlank(message = "La contraseña es obligatoria")
