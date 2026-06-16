2565d60ad4a08f3eaea241e7c38c65aacd28ddff
roquevisciarelli <rvisciarelli2@gmail.com>
2026-06-16 12:16:09 -0300

    Validaciones basicas aulas y materias

--- Aulas/src/main/java/com/TrabajoFinal/Aulas/exceptions/GlobalExceptionHandler.java
+++ Aulas/src/main/java/com/TrabajoFinal/Aulas/exceptions/GlobalExceptionHandler.java
@@ -1,24 +1,32 @@
 package com.TrabajoFinal.Aulas.exceptions;
 
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> manejarValidaciones(MethodArgumentNotValidException ex) {
+        // Obtenemos solo el primer mensaje de error para mostrar un Toast limpio en el frontend
+        String errorMsg = ex.getBindingResult().getFieldErrors().stream()
+                .map(err -> err.getDefaultMessage())
+                .findFirst()
+                .orElse("Error de validación en los datos ingresados");
+
+        return new ResponseEntity<>(errorMsg, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> manejarRuntime(RuntimeException ex) {
-
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> manejarGeneral(Exception ex) {
-
         return new ResponseEntity<>("Ocurrió un error", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
